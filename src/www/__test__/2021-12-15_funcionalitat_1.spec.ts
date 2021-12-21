38694659a19b42cb296865ff139cae6c3a2b0607
gvilaro <59828636+gvilaro@users.noreply.github.com>
2021-12-21 11:00:43 +0100

    Implementació Fase 1

--- src/www/__test__/2021-12-15_funcionalitat_1.spec.ts
+++
@@ -1,4 +0,0 @@
-import { postLineSteps } from "./postLineSteps";
-import { PostTestFactory } from "./testPost/PostTestFactory";
-
-new PostTestFactory().create("2021-12-15_funcionalitat_1", postLineSteps);
--- src/www/__test__/2021-12-21_colonies.spec.ts
+++ src/www/__test__/2021-12-21_colonies.spec.ts
@@ -1,4 +1,4 @@
 import { postLineSteps } from "./postLineSteps";
 import { PostTestFactory } from "./testPost/PostTestFactory";
 
-new PostTestFactory().create("2021-12-15_funcionalitat_3", postLineSteps);
+new PostTestFactory().create("2021-12-21_colonies", postLineSteps);
